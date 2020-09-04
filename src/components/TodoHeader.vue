422d3908b8ffb31053963909a126fe5ac3f16961
2044smile <2044smile@naver.com>
2020-09-04 16:44:28 +0900

    feat(vuex): style

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -1,15 +1,13 @@
 <template>
     <div>
-        header
+        <h1>TODO it!</h1>
     </div>
 </template>
 
-<script>
-export default {
-
+<style scoped> /* 현재 컴포넌트에서만 사용할 수 있는 옵션 */
+h1 {
+    color: #2F3B52;
+    font-weight: 900;
+    margin: 2.5rem 0 1.5rem; /* 폰트의 크기에 따라서 마진이 조정되는 것 */
 }
-</script>
-
-<style>
-
 </style>
