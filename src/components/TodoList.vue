08c5b1ea6e2df2c56bacd849debe03bff0ef8996
2044smile <2044smile@naver.com>
2020-09-06 22:30:56 +0900

    feat: transition very good

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,14 +1,19 @@
 <template>
   <div>
-    <ul>
+    <transition-group name="list" tag="ul">
       <li v-for="(todoItem, index) in propsdata" v-bind:key="todoItem.item" class="shadow">
-        <font-awesome-icon class="fa-2x checkBtn" icon="check" v-bind:class="{checkBtnCompleted: todoItem.completed}" @click="toggleComplete(todoItem, index)" />
+        <font-awesome-icon
+          class="fa-2x checkBtn"
+          icon="check"
+          v-bind:class="{checkBtnCompleted: todoItem.completed}"
+          @click="toggleComplete(todoItem, index)"
+        />
         <span v-bind:class="{textCompleted: todoItem.completed}">{{ todoItem.item }}</span>
         <span class="removeBtn" @click="removeTodo(todoItem, index)">
           <font-awesome-icon class="fa-2x" icon="trash" />
         </span>
       </li>
-    </ul>
+     </transition-group>
   </div>
 </template>
 
@@ -17,12 +22,12 @@ export default {
   props: ["propsdata"],
   methods: {
     removeTodo: function(todoItem, index) {
-      this.$emit('removeItem', todoItem, index);
+      this.$emit("removeItem", todoItem, index);
     },
     toggleComplete: function(todoItem, index) {
-      this.$emit('toggleItem', todoItem, index);
+      this.$emit("toggleItem", todoItem, index);
     }
-  },
+  }
 };
 </script>
 
@@ -58,5 +63,14 @@ li {
 .textCompleted {
   text-decoration: line-through;
   color: #b3adad;
+}
+
+/* 리스트 아이템 트랜지션 효과*/
+.list-enter-active, .list-leave-active {
+  transition: all 0.5s;
+}
+.list-enter, .list-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
 }
 </style>
